7c2246ddb8a233943345f7a9dd720536f81b12ae
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-09-10 20:14:54 -0400

    Added my model and views

---
+++ app/models/category.rb
@@ -0,0 +1,14 @@
+# class Category < ApplicationRecord
+#   belongs_to :author, class_name: 'User'
+#   has_many :categories_purchases, dependent: :destroy, class_name: 'CategoriesPurchases'
+#   has_many :purchases, through: :categories_purchases, dependent: :destroy
+
+#   validates :name, :icon, presence: true
+# end
+
+# category.rb
+class Category < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+    has_many :categories_purchases
+  has_many :purchases, through: :categories_purchases
+end
